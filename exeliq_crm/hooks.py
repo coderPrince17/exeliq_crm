f03c22db42344673f2d114888d933b3dea22c444
coderPrince17 <94529945+coderPrince17@users.noreply.github.com>
2026-06-24 21:59:21 +0530

    Revert "Added Sales Target"
    This reverts commit 4c38a93b1fbd55f8ec1fc0969090a326d51bee23.

--- exeliq_crm/hooks.py
+++ exeliq_crm/hooks.py
@@ -1,12 +1,12 @@
 from . import __version__ as app_version
 
-app_name        = "exeliq_crm"
-app_title       = "Exeliq CRM"
-app_publisher   = "Exeliq Tech Solutions"
+app_name = "exeliq_crm"
+app_title = "Exeliq CRM"
+app_publisher = "Exeliq Tech Solutions"
 app_description = "Exeliq CRM Pipeline Dashboard"
-app_email       = "info@exeliqsolutions.com"
-app_license     = "MIT"
-app_version     = app_version
+app_email = "info@exeliqsolutions.com"
+app_license = "MIT"
+app_version = app_version
 
 required_apps = ["frappe", "erpnext"]
 
@@ -14,8 +14,5 @@ after_install = "exeliq_crm.setup.after_install"
 after_sync    = "exeliq_crm.setup.after_install"
 after_migrate = "exeliq_crm.setup.after_install"
 
+# Load dashboard JS on every desk page
 app_include_js = ["/assets/exeliq_crm/js/crm_dashboard.js"]
-
-permission_query_conditions = {
-	"CRM Sales Target": "exeliq_crm.exeliq_crm.doctype.crm_sales_target.crm_sales_target.get_permission_query_conditions"
-}
